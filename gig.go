de654344b6221ec8c561853cf33c733d3760f654
bdzanko17 <bdzanko1@gmail.com>
2020-01-08 21:28:03 +0100

    Obtained: SRCIP, DESTIP, SRCPRT, DESTPRT, TSVAL, TSCR

--- gig.go
+++ gig.go
@@ -7,19 +7,29 @@ import (
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"log"
+	"net"
 	"time"
 )
 
 var (
-	device       string = "wlp2s0"
+	device       string = "eno1"
 	snapshot_len int32  = 1024
 	promiscuous  bool   = false
 	err          error
 	timeout      time.Duration = 1 * time.Second
 	handle       *pcap.Handle
+	TSval        uint32 = 0
+	TSerc        uint32 = 0
+	source_ip    net.IP
+	dest_ip      net.IP
+	source_port  string
+	dest_port    string
+	srcstr       string
+	dststr       string
 )
 
 func main() {
+
 	// Open device
 	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
 	if err != nil {
@@ -38,17 +48,42 @@ func main() {
 	for packet := range packetSource.Packets() {
 		ipLayer := packet.Layer(layers.LayerTypeIPv4)
 		if ipLayer != nil {
-
+			ip, _ := ipLayer.(*layers.IPv4)
+			source_ip = ip.SrcIP
+			dest_ip = ip.DstIP
+			fmt.Println(source_ip)
+			fmt.Println(dest_ip)
 		}
 		tcpLayer := packet.Layer(layers.LayerTypeTCP)
 		if tcpLayer != nil {
 			tcp, _ := tcpLayer.(*layers.TCP)
 			if len(tcp.Options) >= 3 {
-				fmt.Println(binary.BigEndian.Uint32(tcp.Options[2].OptionData[:4]))
-				fmt.Println(binary.BigEndian.Uint32(tcp.Options[2].OptionData[4:8]))
-			}
 
+				TSval = binary.BigEndian.Uint32(tcp.Options[2].OptionData[:4])
+				TSerc = binary.BigEndian.Uint32(tcp.Options[2].OptionData[4:8])
+				fmt.Println(TSval)
+				fmt.Println(TSerc)
+
+			}
+			source_port = tcp.SrcPort.String()
+			dest_port = tcp.DstPort.String()
+			fmt.Println(source_port)
+			fmt.Println(dest_port)
 		}
+		srcstr = source_ip.String() + ":" + source_port
+		dststr = dest_ip.String() + ":" + dest_port
+		fmt.Println(srcstr)
+		fmt.Println(dststr)
+
+		vrijeme_sat := time.Now().Hour()
+		vrijeme_min := time.Now().Minute()
+		vrijeme_sec := time.Now().Second()
+		vrijeme_nano := float64(time.Now().Nanosecond())
+		vrijeme_string := time.Now().String()
+		vrijeme := vrijeme_sat*3600 + vrijeme_min*60 + vrijeme_sec + int(vrijeme_nano/1e6)
+		fmt.Println("vrijeme:", vrijeme_string)
+		fmt.Println("vrijeme:", vrijeme)
 
 	}
+
 }
