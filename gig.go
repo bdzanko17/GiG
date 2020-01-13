5cab9394e9418fd142b8005a6463920ade8bddb7
Edinpb <prokapb5>
2020-01-13 17:23:48 +0100

    CMD

--- gig.go
+++ gig.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"encoding/binary"
+	"flag"
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"log"
 	"net"
-
 	"time"
 )
 
 var (
-	device       string = "wlp6s0"
+	device       string = "wlp6s0" //default
+	max_pkt      int    = -1       // default
+	max_age      int    = -1       //default
 	snapshot_len int32  = 1024
 	promiscuous  bool   = false
 	err          error
@@ -36,36 +38,22 @@ type Flowrecord struct {
 	tsecr     uint32
 }
 
-func main() {
-	//Getting the IP address of device
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
+func ProcessPacket(handle *pcap.Handle, localAddr string) {
 
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	var start = time.Now()
+	var counter int = 0
+
+	Flow := make(map[string]Flowrecord)
+	RTT_sumary := make(map[string]string)
 
 	fmt.Println("First packet at: ", time.Now())
 
-	Flow := make(map[string]Flowrecord)
-
-	// Open device to start listening
-	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer handle.Close()
-
-	var filter string = "tcp and port 80"
-	err = handle.SetBPFFilter(filter)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Use the handle as a packet source to process all packets
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
+		counter++
+		if (counter > max_pkt && max_pkt != -1) || (int(time.Since(start)) > max_age && max_age != -1) {
+			return
+		}
 		ipLayer := packet.Layer(layers.LayerTypeIPv4)
 		if ipLayer != nil {
 			ip, _ := ipLayer.(*layers.IPv4)
@@ -93,7 +81,9 @@ func main() {
 		srcstr = source_ip.String() + ":" + source_port
 		dststr = dest_ip.String() + ":" + dest_port
 		var fstr string
+
 		fstr = srcstr + dststr
+
 		//Creating structure about Flows and TCP connections
 		x := Flowrecord{
 			last_time: time.Now(),
@@ -105,12 +95,12 @@ func main() {
 		x.flowname = fstr
 		//Checking for bidirectional flow(if yes calculate RTT)
 		_, ok := Flow[dststr+srcstr]
-		if ok && TSerc == Flow[dststr+srcstr].tsval && source_ip.String() != localAddr.IP.String() {
+		if ok && TSerc == Flow[dststr+srcstr].tsval && source_ip.String() != localAddr {
 			var RTT = time.Since(Flow[dststr+srcstr].last_time).String()
 			h, m, s := time.Now().Clock()
-			print(h, ":", m, ":", s, "  KFor flow: ", fstr, " calculated RTT: ")
-			println("RTT: ", RTT)
-			delete(Flow, fstr)
+			RTT_sumary[time.Now().String()] = RTT
+			print(h, ":", m, ":", s, "  For flow: ", dststr+" : "+srcstr, " calculated RTT: ")
+			println(RTT)
 			delete(Flow, dststr+srcstr)
 
 			//IF no insert, Flow in Flowrecords
@@ -123,5 +113,45 @@ func main() {
 		//Back on listening
 
 	}
+}
+
+func main() {
+
+	x := flag.String("i", "wlp6s0", "a string")
+	y := flag.Int("maxp", -1, "an int")
+	z := flag.Int("maxt", -1, "an int")
+	flag.Parse()
+
+	device = *x
+	max_pkt = *y
+	max_age = *z
+
+	fmt.Println("ISPISI STRING: ", max_pkt)
+	fmt.Println("ISPISI STRING: ", max_age)
+	fmt.Println("ISPISI STRING: ", device)
+
+	//Getting the IP address of device
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	// Open device to start listening
+	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	var filter string = "tcp and port 80"
+	err = handle.SetBPFFilter(filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ProcessPacket(handle, localAddr.IP.String()) // Use the handle as a packet source to process all packets
 
 }
